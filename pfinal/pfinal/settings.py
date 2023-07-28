f93d752297ded66f87db0586593a6cd5b42175bb
Francisco Poderoso <franpoderoso@hotmail.com>
2023-07-27 23:33:44 -0300

    vamos bien

--- pfinal/pfinal/settings.py
+++ pfinal/pfinal/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '../static/app/'
+STATIC_URL = r'static/app/'
 #STATICFILES_DIRS = (
 #  os.path.join(BASE_DIR, 'static/app/pagina'),
 #)
