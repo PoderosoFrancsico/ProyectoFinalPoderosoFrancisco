3cbb20935d3e39bdd9aaf4e2897e1db5b4cacf22
Francisco Poderoso <franpoderoso@hotmail.com>
2023-07-29 17:48:11 -0300

    Progress at last

--- pfinal/pagina/urls.py
+++ pfinal/pagina/urls.py
@@ -3,5 +3,7 @@ from pagina import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio.html'),
+    path('blogs', views.blogs, name='blogs.html'),
+    path('about', views.about, name='about.html'),
     
 ]
--- pfinal/pagina/views.py
+++ pfinal/pagina/views.py
@@ -7,4 +7,10 @@ from django.http import HttpResponse
 
 def inicio(request):
     return render(request, 'inicio.html')
+
+def blogs(request):
+    return render(request, 'blogs.html')
+
+def about(request):
+    return render(request, 'about.html')
     
